84faec9ccd5a5f69c697e5dcc4a53acd9d40fdf1
surafelteshale <surafel.teshale@bitscollege.edu.et>
2025-10-29 10:03:10 -0700

    more datatypes

--- database/migrations/2025_10_29_164909_create_posts_table.php
+++ database/migrations/2025_10_29_164909_create_posts_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamps();
 
             // more data types
+            //date, dateTime, time, decimal, double, float, interger,
         });
     }
 
