a6712bd9b434e975dc29aecbb643e039060dda4a
surafelteshale <surafel.teshale@bitscollege.edu.et>
2025-10-29 22:04:21 -0700

    trying crud on controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,37 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(){
-        $blogs = [
-        [
-            'title' => 'Title one',
-            'body' => 'this is a body text',
-            'status' => 1
-        ],
-        [
-            'title' => 'Title two',
-            'body' => 'this is a body text',
-            'status' => 0
-        ],
-        [
-            'title' => 'Title three',
-            'body' => 'this is a body text',
-            'status' => 0
-        ],
-        [
-            'title' => 'Title four',
-            'body' => 'this is a body text',
-            'status' => 1
-        ],
-        [
-            'title' => 'Title five',
-            'body' => 'this is a body text',
-            'status' => 0
-        ],
-    ];
-    return view('home', compact('blogs'));
+
+        // read
+        // return DB::table('posts')->get();
+        // return DB::table('posts')->find(1);
+        // return DB::table('posts')->first();
+        // return DB::table('posts')->pluck('title', 'id');
+        // return DB::table('posts')->where('id', '>=', 0)->where('id', '<', '1')->get();
+
+        // create
+        // DB::table('posts')->insert([
+        //     'title' => 'this is test title',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat unde, sint, voluptatem rem obcaecati consequatur eligendi iste Repellendus, quo? Quibusdam, quisquam ea?',
+        //     'status' => 1,
+        //     'publish_date' => date('y-m-d'),
+        //     'user_id' => 1
+        // ]);
+        // dd('success');
+
+        // update
+        return DB::table('posts')->where('id', 3)->update([
+            'title' => 'we updated the title',
+            'description' => 'this is the updated description'
+        ]);
+
+
+        // return view('home', compact(('blogs')));
     }
 }
